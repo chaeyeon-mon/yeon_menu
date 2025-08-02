946221facfa2172f7c4d6e6f095b86f5d93e2a55
chang heon <koomeho1404@naver.com>
2025-08-02 14:49:38 +0900

    이미지 추가

--- project/src/components/Home.jsx
+++ project/src/components/Home.jsx
@@ -3,10 +3,12 @@ import './Home.css'; // CSS 파일 import
 
 const Home = () => {
   return (
-    <main className="home-container">
-      <div className="content-box">
-        미정
-      </div>
+    <main className="hero-section">
+  <div className="hero-overlay"></div>
+  <div className="hero-content">
+    <h1>AI 기반 의약품 라벨 불량 판정 시스템</h1>
+    <p>실시간 라벨 오류 감지로 품질관리 자동화</p>
+  </div>
     </main>
   );
 };
